f67083f128b5ca989b80f0f3482e2f824d63f3ac
Tejal Limbani <tejal.limbani@technomark.io>
2024-10-18 17:32:07 +0530

    sign in with google chnages

--- src/app/auth-success/__components/auth-success.tsx
+++ src/app/auth-success/__components/auth-success.tsx
@@ -1,22 +1,33 @@
 'use client'
 
+import { handleTokenSave } from '@/actions/server/auth'
 import agent, { invalidateSessionCache } from '@/api/axios'
+import { apUrl } from '@/api/server/common'
 import { handleLoginError } from '@/utils'
+import { encryptToken } from '@/utils/auth'
 import { useSession } from 'next-auth/react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { Loader, Toast } from 'pq-ap-lib'
 import { useEffect, useState } from 'react'
 
 const AuthSuccess = ({ session }: any) => {
-  const user = session ? session?.user : {}
+  // const user = session ? session?.user : {}
   const router = useRouter()
-  const { update } = useSession()
+  // const { update } = useSession()
 
   const [loader, setLoader] = useState(true)
 
-  const searchParams = useSearchParams()
-  const accessToken = searchParams.get('access_token')!
-  const token = searchParams.get('id_token')!
+  let accessToken: string | null = ''
+  let token: string | null = ''
+  if (window.location.hash) {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    accessToken = params.get('access_token');
+    token = params.get('id_token');
+  }
+  // const searchParams = useSearchParams()
+  // const accessToken = searchParams.get('access_token')!
+  // const token = searchParams.get('id_token')!
 
   useEffect(() => {
     const sendToken = async () => {
@@ -25,8 +36,8 @@ const AuthSuccess = ({ session }: any) => {
       try {
         const response = await agent.APIs.socialLogin({
           SocialLoginType: 1,
-          AccessToken: accessToken,
-          Token: token,
+          AccessToken: accessToken ?? '',
+          Token: token ?? '',
         })
 
         setLoader(false)
@@ -35,13 +46,17 @@ const AuthSuccess = ({ session }: any) => {
         if (response.ResponseStatus === 'Success') {
           Toast.success('Success', dataMessage || 'You are successfully logged in.')
 
-          const { Token, RefreshToken } = response.ResponseData
+          const ResponseData = response.ResponseData
+
+          await handleTokenSave({
+            email: ResponseData.Username,
+            access_token: ResponseData.Token,
+            expires_at: ResponseData.TokenExpiry,
+            refresh_token: ResponseData.RefreshToken
+          })
 
           invalidateSessionCache();
-          await update({ ...user, access_token: Token, refresh_token: RefreshToken })
-
-          // api call to check if product selected
-          hasProduct()
+          hasProduct(ResponseData.Token, ResponseData.RefreshToken)
         } else {
           handleLoginError(dataMessage)
           router.push('/signin')
@@ -54,22 +69,17 @@ const AuthSuccess = ({ session }: any) => {
     sendToken()
   }, [accessToken, token])
 
-  const hasProduct = async () => {
-    const response = await agent.APIs.getUserConfig()
+  const hasProduct = async (token: any, refreshToken: any) => {
+    const response = await agent.APIs.getUserProfile()
 
     if (response?.ResponseStatus === 'Success') {
-      const { UserId, OrganizationId, IsAdmin, IsOrganizationAdmin } = response.ResponseData
       const isMapped = response?.ResponseData.products.some((product: any) => product.is_mapped)
 
-      localStorage.setItem('UserId', UserId)
-      localStorage.setItem('OrgId', OrganizationId)
-      localStorage.setItem('IsAdmin', IsAdmin)
-      localStorage.setItem('IsOrgAdmin', IsOrganizationAdmin)
-
       if (isMapped) {
-        router.push('/signin')
+        const encodedToken = encryptToken(encryptToken(token))
+        router.push(`${apUrl}/verify-token?token=${encodeURIComponent(encodedToken)}&refreshToken=${refreshToken}`)
       } else {
-        router.push('/signin')
+        router.push(`/products`)
       }
     }
   }
